3cc60b9807417162ca00ec6db69af9041e1ed0cc
Jenny Morgan <jennymorgna0712@gmail.com>
2023-05-28 18:38:14 -0500

    Initial Commit

---
+++ CeyatiCo/CeyatiCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeyatiCoApp.swift
+//  CeyatiCo
+//
+//  Created by Jenny Morgan Ryan on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CeyatiCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
